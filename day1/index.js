a7687a86008d6548fe71b648cc104d1405659801
dylanb-101 <dylanbarrett360@gmail.com>
2022-12-02 08:54:38 -0500

    day1 part2

--- day1/index.js
+++ day1/index.js
@@ -3,20 +3,36 @@ const fs = require('fs');
 const calories = fs.readFileSync('./day1/calories.txt').toString();
 
 const caloriesArray = calories.split('\n\n');
+let total1 = 0;
+let total2 = 0;
+let total3 = 0;
 let total = 0;
 
-for(eCal in caloriesArray) {
+for(let i = 0; i < 3; i++) {
+
+    for(eCal in caloriesArray) {
     
-    let insideTotal = 0;
+        let insideTotal = 0;
 
-    for(iCal in caloriesArray[eCal].split('\n')) {
+        for(iCal in caloriesArray[eCal].split('\n')) {
 
-        let cals = parseInt(caloriesArray[eCal].split('\n')[iCal]);
+            let cals = parseInt(caloriesArray[eCal].split('\n')[iCal]);
 
-        insideTotal += cals;
+            insideTotal += cals;
 
+        }
+
+
+
+        if(insideTotal > total1 && i == 0) {
+            total1 = insideTotal;
+        } else if(insideTotal > total2 && insideTotal != total1 && i == 1) {
+            total2 = insideTotal;
+        } else if(insideTotal > total3 && insideTotal != total1 && insideTotal != total2 && i == 2){
+            total3 = insideTotal;
+        }
     }
-
-    if(insideTotal > total) total = insideTotal
 }
+total = total1 + total2 + total3;
+
 console.log(total);
